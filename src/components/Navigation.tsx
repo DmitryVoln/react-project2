038394c04e45acab6624ec047c1dd528532836f0
DmitryVoln <DmitryVoln@mail.ru>
2021-09-13 22:17:05 +0300

    app

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom';
 
 export const Navigation = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
-                <div className="collapse navbar-collapse" id="navbarNav">
+        <nav className="navbar">
+            <div className="container">
+                <div>
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <Link to={"/"} className={"nav-link"}>Главная</Link>
+                            <Link to={"/"}>Главная</Link>
                         </li>
                         <li className="nav-item">
-                            <Link to={"/cat"} className={"nav-link"}>Каталог</Link>
+                            <Link to={"/cat"}>Каталог</Link>
                         </li>
                         <li className="nav-item">
-                            <Link to={"/team"} className={"nav-link"}>Команда</Link>
+                            <Link to={"/team"}>Команда</Link>
                         </li>
                     </ul>
                 </div>
